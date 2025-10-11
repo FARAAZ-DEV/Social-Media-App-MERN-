e403e0fc78b746ef5b9c9a9205cdcd7920ad989f
MOHD-FARAZ-01 <muhammadfaraz2105@gmail.com>
2025-10-11 10:09:25 +0530

    Replace bcrypt with bcryptjs for Render deployment

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,29 +1,25 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs'; // use bcryptjs
 import jwt from 'jsonwebtoken';
 import Users from '../models/Users.js';
 
-
-
-
-const generateToken =(id) =>{
-
+// Generate JWT token
+const generateToken = (id) => {
     const jwtSecret = 'thisIsTheSceretCodeForTheJWTToken';
+    return jwt.sign({ id }, jwtSecret, { expiresIn: '30d' });
+};
 
-    return jwt.sign({id}, jwtSecret, {
-        expiresIn: '30d',
-    })
-}
+// Register new user
+export const register = async (req, res) => {
+    try {
+        const { username, email, password, profilePic } = req.body;
 
-export const register = async (req, res) =>{
-    try{
-
-        const {username, email, password, profilePic} = req.body;
-
-        const salt = await bcrypt.genSalt();
+        // Generate salt and hash password
+        const salt = await bcrypt.genSalt(10);
         const passwordHash = await bcrypt.hash(password, salt);
 
+        // Create new user
         const newUser = new Users({
-            username, 
+            username,
             email,
             password: passwordHash,
             profilePic
@@ -31,40 +27,57 @@ export const register = async (req, res) =>{
 
         const user = await newUser.save();
 
-        // generate jwt token using function we defined at top of the page
+        // Generate JWT token
         const token = generateToken(user._id);
 
-        const userData = {_id: user._id, username: user.username,
-                             email:user.email, profilePic:user.profilePic, 
-                             about: user.about, posts: user.posts, 
-                             followers: user.followers, following:user.following };
+        // Prepare user data to send
+        const userData = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic,
+            about: user.about,
+            posts: user.posts,
+            followers: user.followers,
+            following: user.following
+        };
 
-        res.status(200).json({token, user:userData});
+        res.status(200).json({ token, user: userData });
 
-    }catch(err){
-        res.status(500).json({error: err.message});
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
 
-export const login = async (req, res) =>{
-    try{
-        const {email, password} = req.body;
-        const user = await Users.findOne({email:email});
-        if(!user) return res.status(400).json({msg: "User does not exist"});
+// Login user
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
+        const user = await Users.findOne({ email: email });
+        if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+        // Compare password using bcryptjs
         const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) return res.status(400).json({msg: "Invalid credentials"});
+        if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
 
-        // generate jwt token using function we defined at top of the page
+        // Generate JWT token
         const token = generateToken(user._id);
-        delete user.password;
-        const userData = {_id: user._id, username: user.username, email:user.email,
-                             profilePic:user.profilePic, about: user.about, posts: user.posts,
-                             followers: user.followers, following:user.following };
-                             
-        res.status(200).json({token, user:userData});
-        console.log(token, userData);
-    }catch(err){
-        res.status(500).json({error: err.message});
+
+        const userData = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            profilePic: user.profilePic,
+            about: user.about,
+            posts: user.posts,
+            followers: user.followers,
+            following: user.following
+        };
+
+        res.status(200).json({ token, user: userData });
+
+    } catch (err) {
+        res.status(500).json({ error: err.message });
     }
 };
